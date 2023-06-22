6611adbe38fece3a9db2e45eedd15150eb5c7fbc
Frey <Frey0xd@gmail.com>
2023-06-22 14:26:46 -0400

    GYM

---
+++ trash.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Gym {
+  protected:
+    int weeks;
+  public:
+    void SetBody(int a) {
+      weeks=a;
+
+    };
+
+class UpperBody: public Gym {
+  public:
+    void workout ()
+    { count << "The Body you are seeking for! << weeks << endl; }
+};
+
+class Zyzz: public Gym {
+  public 
