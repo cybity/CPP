1c3d29185e903ff69fe60d7ec1b049077785eaf3
Frey <Frey0xd@gmail.com>
2023-06-23 12:42:41 -0400

    GYM

--- trash.cpp
+++ trash.cpp
@@ -13,8 +13,23 @@ class Gym {
 class UpperBody: public Gym {
   public:
     void workout ()
-    { count << "The Body you are seeking for! << weeks << endl; }
+    { count << "The Body you are seeking for!" << weeks << endl; }
 };
 
 class Zyzz: public Gym {
-  public 
+  public:
+    void workout()
+    { cout << "For LowerBody" << weeks <<endl; }
+};
+
+int main() {
+  UpperBody u; 
+  Zyzz z;
+  Gym *gym1 = &u; // Sorry I have made this complex but its just address
+  Gym *gym2 = &z; // All to zyzz who put me into BodyBuilding 
+  gym1->setweeks(6); // sunday = Rest bruh 
+  gym2->setweeks(7); // if you do cardio and wants to die 
+  u.workout();
+  m.attack();
+}
+
